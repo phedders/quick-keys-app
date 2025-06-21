ea59cf4794448ffbed0a1211461f4df0cb7a88ea
Paul Hedderly <paul+git@mjr.org>
2025-06-21 20:14:47 +0100

    prh: vol down/up improvement

--- scripts/vol_down.sh
+++ scripts/vol_down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-SINK=$(pactl list short sinks | cut -f 2)
+SINK=$(pactl list short sinks | cut -f 2|tail -1)
 
-pactl set-sink-volume alsa_output.pci-0000_0c_00.1.hdmi-stereo-extra4 -2% > /dev/null 2>&1
+pactl set-sink-volume ${SINK} -2% > /dev/null 2>&1
--- scripts/vol_up.sh
+++ scripts/vol_up.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-pactl set-sink-volume alsa_output.pci-0000_0c_00.1.hdmi-stereo-extra4 +2% > /dev/null 2>&1
+
+SINK=$(pactl list short sinks | cut -f 2|tail -1)
+
+pactl set-sink-volume ${SINK} +2% > /dev/null 2>&1
